690d47ce73567bdf92f3e290fcb8e313bd68c189
Thoughtless Labs <john@thoughtlesslabs.com>
2021-06-01 15:46:59 -0400

    Building a test module

---
+++ Udemy Course/my_main_package/subpackage/my_subscript.py
@@ -0,0 +1,2 @@
+def sum_report():
+    print("Hey! I'm a function inside my subscript")
---
+++ Udemy Course/my_program.py
@@ -0,0 +1,5 @@
+from my_main_package import main_script
+from my_main_package.subpackage import my_subscript
+
+main_script.report_main()
+my_subscript.sum_report()
