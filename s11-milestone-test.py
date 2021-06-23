a85e4fc770fdf57a575b3e322f18fd5ce6e831aa
Thoughtless Labs <john@thoughtlesslabs.com>
2021-06-23 10:32:19 -0400

    Adding in user input and game logic

--- s11-milestone-test.py
+++ s11-milestone-test.py
@@ -28,7 +28,7 @@ import random
 
 suits = ('Hearts','Diamonds','Spades','Clubs')
 ranks= ('Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace')
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven': 7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven': 7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
 
 class Card():
 
@@ -69,32 +69,60 @@ class Player():
     def stand():
         pass
 
+    def __string__(self):
+        pass
+
+#create player and dealer
+player = Player(input('What is your name? '))
+dealer = Player('Dealer')
+
 #create a new deck
 game_deck = Deck()
 
 #shuffle the deck
 game_deck.shuffle()
 
-#create player and dealer
-player = Player(input('What is your name? '))
-dealer = Player('Dealer')
-
 game_on = True
 while game_on:
-    
+    player_score = 0
+    dealer_score = 0
+
+
     if len(game_deck.all_cards) < 5:
         #create a new deck
         game_deck = Deck()
 
         #shuffle the deck
         game_deck.shuffle()
-
     
+    playing = True
 
-    #Player's Turn
-    player_turn = True
-    while player_turn:
-        print(f"{player.name}'s turn")
-        break
+    while playing:
+        for i in range(2):
+            player.add_cards(game_deck.deal_one())
+            dealer.add_cards(game_deck.deal_one())
+
+        print(len(player.all_cards))
+
+        for i in range(0,len(player.all_cards)):
+            player_score += player.all_cards[i].value
+
+        for i in range(0,len(dealer.all_cards)):
+            dealer_score += dealer.all_cards[i].value
+
+
+        if player_score == 21:
+            print(f"{player.name} has Blackjack!")
+        elif dealer_score == 21:
+            print(f"{dealer} has Blackjack! {player.name} loses.")
+        playing = False
+    else:
+        #Player's Turn
+        player_turn = True
+        while player_turn:
+            print(f"{player.name}'s turn")
+            print(f"{player.name} has:\n {player.all_cards[0]}\n {player.all_cards[1]}\n ")
+            print(f"Do you want to hit ")
+            break
 
     game_on = False
